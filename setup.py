e49c22fce423f76d5b1bb93eaa7d3cadd5aee901
martins0n <martinmail@mail.ru>
2020-01-17 21:17:40 +0300

    requires added

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup, find_packages
 
 
+install_requires=[
+   'ImageHash>=4.0',
+   'numpy>=1.17.4',
+   'Pillow>=6.2.1',
+   'scikit-image>=0.16.2',
+   'scipy>=1.3.3',
+   'tensorly>=0.4.5'
+]
+
+
 setup(
     name='tuckerhash',
     version='0.0.1',
     description='Tucker decomposition for image hashing',
     url='https://github.com/martins0n/nla_project_image_hash',
-    packages=find_packages(exclude=('tests'))
+    packages=find_packages(exclude=('tests')),
+    install_requires=install_requires
 )
